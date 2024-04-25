b795f1fbc764b672905b35766a633dabda9093f4
Cansel Karabulut <canselkarabulutt@gmail.com>
2024-04-25 23:10:11 +0300

    main

---
+++ Assets/Scripts/Stitch/BackGround.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BackGround : Tumbnails
+{
+    public GameObject imagePrefabInstantiate;
+    public GameObject parentInstantiate;
+
+    private void Start()
+    {
+        Stitch(imagePrefabInstantiate, parentInstantiate,0,23,0,23);
+    }
+   
+}
