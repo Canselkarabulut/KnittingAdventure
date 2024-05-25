357fa95513ba06f552b9018fd8a3870648d58163
Cansel Karabulut <canselkarabulutt@gmail.com>
2024-05-26 02:40:10 +0300

    wool view

--- Assets/Scripts/GamePlay/Wool/Colors.cs
+++ Assets/Scripts/GamePlay/Wool/Colors.cs
@@ -9,48 +9,7 @@ public class Colors : MonoBehaviour
     public Image stitchPrefab;
     public Color32 color = new Color(1, 0.9686275f, 0, 1);
    
-
-    public void YellowButton()
-    {
-        stitchPrefab.color = color;
-    }
-    public void RedButton()
-    {
-        stitchPrefab.color = color;
-    }
-    public void BlueButton()
-    {
-        stitchPrefab.color = color;
-    }
-    public void GreenButton()
-    {
-        stitchPrefab.color = color;
-    }
-    public void BrownButton()
-    {
-        stitchPrefab.color = color;
-    }
-    public void WhiteButton()
-    {
-        stitchPrefab.color = color;
-    }
-    public void BlackButton()
-    {
-        stitchPrefab.color = color;
-    }
-    public void PinkButton()
-    {
-        stitchPrefab.color = color;
-    }
-    public void PurpleButton()
-    {
-        stitchPrefab.color = color;
-    }
-    public void OrangeButton()
-    {
-        stitchPrefab.color = color;
-    }
-    public void LightBlueButton()
+    public void ColorsButton()
     {
         stitchPrefab.color = color;
     }
