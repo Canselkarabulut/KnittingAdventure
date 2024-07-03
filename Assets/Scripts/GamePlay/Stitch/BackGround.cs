e4a709f7ea080934ce683f26180cd46985f79520
Cansel Karabulut <canselkarabulutt@gmail.com>
2024-07-03 14:54:29 +0300

    mini level

--- Assets/Scripts/GamePlay/Stitch/BackGround.cs
+++ Assets/Scripts/GamePlay/Stitch/BackGround.cs
@@ -10,16 +10,18 @@ public class BackGround : Tumbnails
 {
     public GameObject imagePrefabInstantiate;
     public GameObject parentInstantiate;
-
+    public Image miniImage;
+    
     public List<Color> colorArrayList;
-
     // public Image desiredImage;
     // public List<Sprite> sprites;
     public List<Texture2D> levelTexture2d;
+    public List<Texture2D> miniImageLevel;
 
     public void DesiredStitchLevel(int textureIndex)
     {
         Stitch(imagePrefabInstantiate, parentInstantiate, 0, 22, 0, 22, levelTexture2d[textureIndex], colorArrayList);
+        miniImage.sprite = miniImage.sprite = Sprite.Create(miniImageLevel[textureIndex], new Rect(0, 0, miniImageLevel[textureIndex].width, miniImageLevel[textureIndex].height), Vector2.zero);;
     }
 
    // public void DesiredStitchLevel1()
